b61fa7091d8e90ad100f763e7688b0deeb63e1ed
gomezfarm <gomez.farm@outlook.com>
2025-04-23 07:51:49 -0600

    Botones de la calculadora

--- 03-calculator-app/app/index.tsx
+++ 03-calculator-app/app/index.tsx
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { globalStyles } from '@/styles/global-styles'
 import ThemeText from '@/components/ThemeText'
+import CalculatorButton from '@/components/CalculatorButton'
+import { Colors } from '@/constants/Colors'
 
 const CalculatorApp = () => {
 
@@ -21,16 +23,136 @@ const CalculatorApp = () => {
   */
   return (
     <View style={globalStyles.calculatorContainer}>
-      <ThemeText
-        variant='h1'
-      >
-        50 x 50
-      </ThemeText>
 
-      <ThemeText 
-        variant='h2'
-      >250
-      </ThemeText>
+      {/* 
+        Metemos los textos dentro del View que seria como una <li> en HTML
+        y les aplicas estos estilos para que no esten muy pegados a la orilla
+
+        Estos son los resultados donde se mostrara cuando se precione un numero de la calculador y el resultado
+      */}
+      <View style={{ paddingHorizontal: 30, marginBottom: 20 }}>
+        <ThemeText
+          variant='h1'
+        >
+          50 x 50
+        </ThemeText>
+
+        <ThemeText 
+          variant='h2'
+        >250
+        </ThemeText>
+      </View>
+
+      {/* Estos son las filas de botones 
+        Por la forma en la que son los botones, son un fuerte candidato para crearlos como un componente personalizado ya que se repite todo
+        solo cambia el contenido y el color de fondo y solo uno el tamano
+
+        Este estilo que le especificamos aqui es para poder colocar los elementos internamente 
+      */}
+      <View style={ globalStyles.row }>
+        <CalculatorButton 
+          label='C'
+          blackText // Con solo mandarlo asi seria a tener la property como True
+          color={ Colors.lightGray }
+          onPress={ () => console.log('c') }
+        />
+        <CalculatorButton 
+          label='+/-'
+          blackText
+          color={ Colors.lightGray }
+          onPress={ () => console.log('+/-') }
+        />
+        <CalculatorButton 
+          label='del'
+          blackText
+          color={ Colors.lightGray }
+          onPress={ () => console.log('del') }
+        />
+        <CalculatorButton 
+          label='÷'
+          blackText
+          color={ Colors.orange }
+          onPress={ () => console.log('Divicion') }
+        />
+      </View>
+      <View style={ globalStyles.row }>
+        <CalculatorButton 
+          label='7'
+          onPress={ () => console.log('7') }
+        />
+        <CalculatorButton 
+          label='8'
+          onPress={ () => console.log('8') }
+        />
+        <CalculatorButton 
+          label='9'
+          onPress={ () => console.log('9') }
+        />
+        <CalculatorButton 
+          label='X'
+          blackText
+          color={ Colors.orange }
+          onPress={ () => console.log('X') }
+        />
+      </View>
+      <View style={ globalStyles.row }>
+        <CalculatorButton 
+          label='4'
+          onPress={ () => console.log('4') }
+        />
+        <CalculatorButton 
+          label='5'
+          onPress={ () => console.log('5') }
+        />
+        <CalculatorButton 
+          label='6'
+          onPress={ () => console.log('6') }
+        />
+        <CalculatorButton 
+          label='-'
+          blackText
+          color={ Colors.orange }
+          onPress={ () => console.log('-') }
+        />
+      </View>
+      <View style={ globalStyles.row }>
+        <CalculatorButton 
+          label='1'
+          onPress={ () => console.log('1') }
+        />
+        <CalculatorButton 
+          label='2'
+          onPress={ () => console.log('2') }
+        />
+        <CalculatorButton 
+          label='3'
+          onPress={ () => console.log('3') }
+        />
+        <CalculatorButton 
+          label='+'
+          blackText
+          color={ Colors.orange }
+          onPress={ () => console.log('+') }
+        />
+      </View>
+      <View style={ globalStyles.row }>
+        <CalculatorButton 
+          label='0'
+          doubleSize // Este boton tiene un tamano diferent al resto
+          onPress={ () => console.log('0') }
+        />
+        <CalculatorButton 
+          label='.'
+          onPress={ () => console.log('.') }
+        />
+        <CalculatorButton 
+          label='='
+          blackText
+          color={ Colors.orange }
+          onPress={ () => console.log('=') }
+        />
+      </View>
+
     </View>
   )
 }
---
+++ 03-calculator-app/components/CalculatorButton.tsx
@@ -0,0 +1,46 @@
+ import { Colors } from '@/constants/Colors'
+import { globalStyles } from '@/styles/global-styles'
+import { Text, Pressable } from 'react-native'
+ 
+ // Como el texto es personalidao lo podemos recibir como un Children como una propeetie
+ // Tambien el color y la forma es lo que varia para cada boton por eso las definimos aqui
+ interface Props{
+    label: string,
+    color?: string,
+    blackText?: boolean, // algunos tendran el texto en negro y por defecto esta en blanco
+    doubleSize?: boolean,
+    onPress: () => void,
+ }
+
+ // Componente personalizado para crear a partir de aqui los demas botones de la calculadora 
+ const CalculatorButton = ({ 
+    label, 
+    color = Colors.darkGray, // Al ser valores opcionales le declaramos valores por defecto
+    blackText = false, 
+    doubleSize= false,
+    onPress 
+}: Props) => {
+   return (
+    <Pressable 
+        // En lugar de mandar los estilos directamante vamos a mandar una funcion que nos va a regresar inmediatamente el objeto de los estilos
+        // Asi podemos desestructurar el pressed con el cual podemos darle el efecto que se preciono el boton
+        style={ ({ pressed }) => ({
+            ...globalStyles.button,
+            backgroundColor: color, // Le pasamos el color que recibimos en el Prop
+            opacity: pressed ? 0.8 : 1, // Darle el efecto que se toco el boton
+            width: doubleSize ? 180 : 80, // Si es True le cambiamos el tamano a 180
+        })}
+        onPress={ onPress }
+    >
+        <Text 
+            style={{ 
+                ...globalStyles.buttonText, // Esparcimos todas las properties que tenfa el global Styles para lo que pongamos despues lo sobrescriba
+                color: blackText ? 'black' : 'white', // Si es True sera negro sino blanco
+
+            }}
+        >{ label }</Text>
+    </Pressable>
+   )
+ }
+ 
+ export default CalculatorButton
