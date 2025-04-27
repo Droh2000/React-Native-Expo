70cfade3f3f945fd96e22e939a13dd9a5c2baf0b
gomezfarm <gomez.farm@outlook.com>
2025-04-27 16:09:22 -0600

    Seccion 6: Tipos de navegacion y Estilos

---
+++ 04-navigation-app/app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+
+// Este es nuestro punto de entrada en la aplicacion
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default index
+// Cualquier archivo que pongamos dentro de la carpeta "app" va a terminar siendo una pantalla
+// esto seria un componente que ocuparia todo el tamano de la pantalla del dispositivo
