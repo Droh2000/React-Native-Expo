08c4ba4b033be0824493cfcf948703095438e6cc
gomezfarm <gomez.farm@outlook.com>
2025-05-25 17:59:37 -0600

    Cambiar titurlo basado en los argumentos

--- 04-navigation-app/app/(drawer)/(tabs)/(stack)/products/[id].tsx
+++ 04-navigation-app/app/(drawer)/(tabs)/(stack)/products/[id].tsx
@@ -2,16 +2,26 @@
 // que reciba de forma dinamica el ID, siempre cuando se entre en la carpeta "products"
 // se va a ir al index.tsx pero si queremos ir a este directorio y mandamos un argumento se iria a este archivo
 import { products } from '@/store/products.store';
-import { Redirect, useLocalSearchParams } from 'expo-router'
+import { Redirect, useLocalSearchParams, useNavigation } from 'expo-router'
+import { useEffect } from 'react';
 import { View, Text } from 'react-native'
 
 const ProductScreen = () => {
 
     // Esta pantalla recibe el ID del producto, de esta funcion extraemos lo que nos interesa
     const {id} = useLocalSearchParams();
+    // Para cambiar el titulo de la pagina ocupamos el navigation
+    const navigation = useNavigation();
 
     // Buscamos el producto por el Id
     const product = products.find(p => p.id === id);
+
+    // Aqui podemos determinar cual es el titulo que tendra la pagina
+    useEffect(() => {
+        navigation.setOptions({
+            title: product?.title ?? 'Producto',
+        });
+    }, [product]);
 
     // Verificamos si no existe entonces mandamos al usuario a la pagina de inicio
     if(!product){
