a9543b3328b824e439e9a3b2ed5dc848c59dfa09
gomezfarm <gomez.farm@outlook.com>
2025-05-27 14:23:01 -0600

    Seccion 8: MoviesApp - Peticiones HTTP - TanStack

---
+++ 05-movies-app/app/_layout.tsx
@@ -0,0 +1,12 @@
+import "../global.css";
+import { View, Text } from 'react-native';
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text>RootLayout</Text>
+    </View>
+  )
+}
+
+export default RootLayout
---
+++ 05-movies-app/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: [
+    "./App.tsx", 
+    "./components/**/*.{js,jsx,ts,tsx}",
+    // Aqui agregamos esta estructura de directorios de nuestro proyecto
+    "./presentation/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
