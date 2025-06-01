50e8defc2b225cdb2583802d8c2744aed75cf446
gomezfarm <gomez.farm@outlook.com>
2025-06-01 09:54:44 -0600

    Determinar Fin del Scroll del FlatList

--- 05-movies-app/presentation/components/movies/MovieHorizontalList.tsx
+++ 05-movies-app/presentation/components/movies/MovieHorizontalList.tsx
@@ -1,15 +1,47 @@
 import { Movie } from '@/infraestructure/interfaces/movie.interface';
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList, NativeScrollEvent, NativeSyntheticEvent } from 'react-native'
 import MoviePoster from './MoviePoster';
+import { useRef } from 'react';
 
 interface Props {
     // Como es un componente reutilizable se podra indicar cual es el titulo a mostrar
     title?: string;
     movies: Movie[];
     className?: string; // Para poder agregarle estilos
+    loadNextPage?: () => void,
 }
 
-const MovieHorizontalList = ({ title, movies, className }: Props) => {
+const MovieHorizontalList = ({ title, movies, className, loadNextPage }: Props) => {
+
+  // Como el evento de "onScroll" es muy ruidiso y emite muchos valores, hay que evitar llamarlo si hay algun tipo de procesamiento
+  const isLoading = useRef(false);
+
+  // Logica para detectar cuando movemos el FlatList
+  // Este tipado lo sacamos al poner el cursor ensima del "event" en el onScroll del FlatList
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    // Si estamos cargando o mandando a llamar una accion evitamos que ejecute este metodo
+    if( isLoading.current ) return;
+
+    // Para Determinar el final del scroll ocupamos desestructurar esto
+    // contentOffset:     Es la posicion en la que nos encontramos en el Scroll
+    // layoutMeasurement: Es lo que vemos en pantalla
+    // contentSize:       Este es el contenido de lo que tenemos
+    const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
+
+    // Sacamos cuando llegamos al final o estamos cerca del final
+    // usamos la X porque es un scroll horizontal el que tenemos no vertical, el 600 es para agregarle un marge extra antes de que llegue al final
+    const isEndReached = (contentOffset.x + layoutMeasurement.width + 600) >= contentSize.width;
+
+    // Si no llegamos al final no hay nada que hacer
+    if( !isEndReached ) return;
+
+    // Si llegamos al final, evitamos que si vuelve a mover el scroll dispare todo este procedimiento
+    isLoading.current = true;
+
+    // Cargamos las siguientes peliculas, si tiene algun valor esta funcion la mandamos a llamar
+    loadNextPage && loadNextPage();
+  }
+
   return (
     // Lo agregamos aqui por si en el futuro queremos expandirlo, es mas facil lograrlo aqui
     <View className={ `${className}` }>
@@ -26,6 +58,8 @@ const MovieHorizontalList = ({ title, movies, className }: Props) => {
         // Este es para indicarle a React cuando un elemento cambia en base a su ID
         keyExtractor={(item) => `${ item.id }`}
         showsHorizontalScrollIndicator={false}
+        // Tenemos un evento que es emitido cada vez que hacemos scroll en la lista (Este evento emite muchos valores)
+        onScroll={ onScroll }
       />
     </View>
   )
