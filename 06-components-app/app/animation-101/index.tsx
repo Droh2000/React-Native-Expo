63f7e8ed675daca4af8e4053ae5cf5d52ed5eb6c
gomezfarm <gomez.farm@outlook.com>
2025-06-14 14:26:04 -0600

    Animaciones 101 - Movimiento

--- 06-components-app/app/animation-101/index.tsx
+++ 06-components-app/app/animation-101/index.tsx
@@ -1,7 +1,7 @@
 import ThemedButtom from '@/presentation/shared/ThemedButtom';
 import ThemedView from '@/presentation/shared/ThemedView';
 import { useRef } from 'react';
-import { Animated, View } from 'react-native';
+import { Animated, Easing, View } from 'react-native';
 
 const Animationsw101Screen = () => {
 
@@ -9,6 +9,10 @@ const Animationsw101Screen = () => {
   // Esta constante nos permite controlar la opacidad y usamos "useRef" porque no requerimos que cada vez que cambie su valor
   // renderize nuevamente el elemento y creamos un Valor animado que va de 0 al valor que queramos
   const animatedOpacity = useRef( new Animated.Value(0) ).current;
+
+  // Queremos hacer que cuando precionamos el boton la caja caiga desde arriba hacia abajo
+  // El -100 es la cantidad de pixeles que tendra 
+  const animatedTop = useRef( new Animated.Value(-100) ).current;
 
   // Para hacer un FadeIn tenemos que pasar del valor 0 al 1, suponiendo que 0 es la opacidad
   const fadeIn = () => {
@@ -20,6 +24,18 @@ const Animationsw101Screen = () => {
       useNativeDriver: true // para que este acelerado por hardwared
       // Para que funcione tenemos que llamar el metodo start
     }).start();
+
+    Animated.timing( animatedTop, {
+      // Este es hacia donde queremos mover el animatedTop
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      // Esto es para indicar como queremos que los valores cambien a lo largo del tiempo que dure la animacion
+      // Aqui estamos diciendo que tenga un efecto como gelatina
+      //  easing: Easing.elastic(3)
+      // Esto es para que tenga un efecto de rebote
+      easing: Easing.bounce
+    }).start();
   }
 
   const fadeOut = () => {
@@ -27,7 +43,12 @@ const Animationsw101Screen = () => {
       toValue: 0,
       duration: 300,
       useNativeDriver: true
-    }).start();
+      // Queremos que tan pronto termine esta animacion resetemos el valor de la aminacion FadeIn, esta funcion Start recibe un Callback que es la funcion
+      // que se va a disparar cuando la aimacion termine, esto no permite concatenar animacion
+      // y asi ponemos que el cuadrado se ponga arriba
+    //        }).start(() => animatedTop.setValue(-100) );
+    // Si tubieramos mas animaciones concatenadas con esta funcion las resetamos todas a su valor original 
+    }).start(() => animatedTop.resetAnimation() );
   }
 
   return (
@@ -42,6 +63,12 @@ const Animationsw101Screen = () => {
           width: 150,
           height: 150,
           opacity: animatedOpacity, // Si queremos hacer invicible el objeto ponemos la opacidad en 0
+          // Podriamos pensar en que con la propiedad Top se le pasaria el animatedTop pero no funcionara porque esto afecta a los elementos hijos fisicamente
+          // y lo que queremos es cambiarlo sin afectar el entorno, para eso usamos el Transform
+          transform: [{
+            // Como queremos mover de arriba a abajo
+            translateY: animatedTop
+          }]
         }}
       />
 
