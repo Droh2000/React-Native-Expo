f62f9bbe13b83a6ab84bd2f3a063b3b69b659134
gomezfarm <gomez.farm@outlook.com>
2025-06-22 12:38:04 -0600

    ThemeChanger - Que el usuario pueda cambiar de tema segun sus preferencias

--- 06-components-app/app/themes/index.tsx
+++ 06-components-app/app/themes/index.tsx
@@ -1,10 +1,60 @@
-import { View, Text } from 'react-native';
+import ThemedCard from '@/presentation/shared/ThemedCard';
+import ThemedSwitch from '@/presentation/shared/ThemedSwitch';
+import ThemedView from '@/presentation/shared/ThemedView';
+import { useState } from 'react';
+//import { useColorScheme } from 'react-native';
+import { useColorScheme } from 'nativewind';
 
+// Vamos a hacer que el usuario puedo elegir y cambiar entre el modo Dark y Light segun sus preferencias
 const ThemesScreen = () => {
+
+  // Detrminamos si estamos en modo Light o Dark
+  //  const theme = useColorScheme();
+  // Gracias a NativeWind tenemos implementada la siguiente logica
+  const { colorScheme, setColorScheme } = useColorScheme();
+
+  const [darkModeSettings, setDarkModeSettings] = useState({
+    // darkMode: theme === 'dark',
+    darkMode: colorScheme === 'dark',
+    systemMode: false,
+  });
+
+  const setDarkMode = ( value: boolean ) => {
+
+    // Establecemos el tema
+    setColorScheme( value ? 'dark' : 'light' );
+
+    setDarkModeSettings({
+      darkMode: value,
+      systemMode: false, // Si la persona lo queire cambiar manualmente entonces no quiere que este basado en el sistema
+    });
+  }
+
+  const setSystemMode = ( value: boolean ) => {
+    setDarkModeSettings({
+      darkMode: darkModeSettings.darkMode,
+      systemMode: value, // Siempre sera prioritado al que este en true
+    });
+  }
+
   return (
-    <View>
-      <Text>ThemesScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedCard className='mt-5'>
+        <ThemedSwitch
+          text='Dark Mode'
+          className='mb-5'
+          value={ darkModeSettings.darkMode }
+          onValueChange={ setDarkMode }
+        />
+        <ThemedSwitch
+          text='System Mode'
+          value = { darkModeSettings.systemMode }
+          onValueChange={ setSystemMode }
+        />
+      </ThemedCard>
+    </ThemedView>
   );
+  // En este punto si salimos de la aplicacion y volvemos a entrar no se guardaran los cambios que pusimos, tenemos que almacenar
+  // las preferencias en algun lugar para que se mantengan las configuraciones que el usuario establecio
 };
 export default ThemesScreen;
